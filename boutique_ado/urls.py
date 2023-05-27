cc8d5790800f5adde05a85eb902c7150cf026d21
Shizuka Donaghue <sdonaghue@gmail.com>
2023-05-27 06:48:12 +0000

    Correct urls.py for handler 404

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'boutique_ado.views.handler404'
